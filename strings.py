060846f6703cca964cfea8e648ac8a30d7906450
AkoBanul20 <AkoBanul20@gmail.com>
2020-04-20 06:47:11 +0800

    First Commit

---
+++ strings.py
@@ -0,0 +1,24 @@
+#String
+pangalan = 'Aaron Jerish Caimbon'
+edad = 23
+city = 'Quezon City'
+
+''' concatenate ''' 
+#print('Ako si ' + pangalan + ', ' + str(edad) + ' taong gulang')
+
+''' String Formatting '''
+#Positional Argument
+#print('Ako si {name}, {age} taong gulang taga {city}'.format(name=pangalan,age=edad,city='Quezon City'))
+
+#F-String
+#print(f'Ako si {pangalan}, {edad} taong gulang taga {city}')
+
+
+''' String methods '''
+
+BC = 'Welcome To Our Youtube Channel'
+
+New_BC = BC.endswith('l')
+print(New_BC)
+
+
